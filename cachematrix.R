f55d7493dee38993cec8525e4f9eca273a22bd9b
Cbanzaime23 <31398258+Cbanzaime23@users.noreply.github.com>
2017-12-17 11:54:00 +0800

    Add files via upload
    My 2nd Programming Assignment

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,46 @@
-## Put comments here that give an overall description of what your
-## functions do
+#Concept of Lexical Scopping
 
-## Write a short comment describing this function
 
+# makeCacheMatrix = This function will solve for 
 makeCacheMatrix <- function(x = matrix()) {
+        # create a matrix object x and some associated sub-functions/methods
+        
+        # define the cache m
+        m <- NULL
+        set <- function(y) {
+                x <<- y 
+                # assign the input matrix y to the variable x 
+                
+                m <<- NULL # re-set m in the parent  to null
+        }
+        get <- function() x # return the matrix x
+        setinverse <- function(inverse) m <<- inverse 
+        
+        getinverse <- function() m #return the inversed cache of x
+        list(set = set, get = get,
+             setinverse = setinverse,
+             getinverse = getinverse)
 
 }
 
 
-## Write a short comment describing this function
+# cacheSolve function which check if  inverse has been already calculated , extracting it from the cache, or else, the inverse will be calculated and stored it for future usage. we need to do this because calculating the inverse of a matrix is computationally expensive
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve<- function(x, ...) {
+        ## Return a matrix that is the inverse
+        
+        m <- x$getinverse()
+        #If m is already calculated, don't recalculate, retrieve its value
+        
+        if(!is.null(m)) {
+                message("getting cached data")
+                return(m)
+                
+                
+        }
+        data <- x$get()
+        m <- solve(data, ...)
+        x$setinverse(m)
+        #Return m
+        m
 }
